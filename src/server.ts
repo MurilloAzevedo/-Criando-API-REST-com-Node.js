f08773cc5dfe01e01d7933cc6dcafc47d3e2f587
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-09-16 20:01:13 -0300

    Apredendo Queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database.js'
+import { title } from 'node:process'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex ('transactions').select('*')
 
-  return tables
+  return transactions
 })
 
 app.listen({
