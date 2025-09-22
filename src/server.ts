1850913be178529a3694013cfa1ae2837f6abb8e
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-09-22 20:02:31 -0300

    criação de transactions, usando rotas

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,12 @@ import crypto from 'node:crypto'
 import { knex } from './database.js'
 import { title } from 'node:process'
 import { env } from './env/index.js'
+import { transactionsRoutes } from './routes/transactions.js'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex ('transactions').select('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app.listen({
